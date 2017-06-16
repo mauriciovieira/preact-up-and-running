1dd3aa421c21194a1d61650e4785e76f63cac367
Mauricio Vieira <mauricio@mauriciovieira.net>
2017-06-16 11:32:46 -0300

    Implement reusable components
    - no css

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import { h } from 'preact'
+import User from './User'
 
+const users = [
+    {
+        image: "https://avatars3.githubusercontent.com/u/95258?v=3&s=460",
+        name: "Mauricio Vieira"
+    },
+    {
+        image: "https://avatars3.githubusercontent.com/u/19439353?v=3&s=460",
+        name: "Elizabeth Ramos"
+    },
+]
 function App () {
     return (
         <div class="app">
-            <h1>Hello JSX</h1>
+            {users.map(user => <User {...user} key={user.name} />)}
         </div>
     )
 }
